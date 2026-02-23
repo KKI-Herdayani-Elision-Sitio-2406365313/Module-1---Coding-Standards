4926c41cb41317a3da684e8ce195bc4562058461
Herdayani Elision Sitio <herdayanisitio@Herdayanis-MacBook-Pro.local>
2026-02-24 04:18:14 +0700

    springboot change version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,9 @@ plugins {
 	jacoco
 	pmd
 
-	id("org.springframework.boot") version "3.2.2"
+	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
+
 }
 
 pmd {
