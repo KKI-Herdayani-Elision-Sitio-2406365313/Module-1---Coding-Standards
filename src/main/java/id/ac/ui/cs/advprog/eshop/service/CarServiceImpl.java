5db24373450d367794c89b7a988f5526d7f9a9ad
Herdayani Elision Sitio <herdayanisitio@Herdayanis-MacBook-Pro.local>
2026-03-10 13:56:44 +0700

    [RED] Add Order model skeleton

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -42,7 +42,6 @@ public class CarServiceImpl implements CarService {
     @Override
     public void deleteCarById(String carId) {
         // TODO Auto-generated method stub
-
         carRepository.delete(carId);
     }
 }
